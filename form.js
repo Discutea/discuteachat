e8385464513825b28e5188e96fbdc7beabbffb3e
David <contact@discutea.com>
2017-03-10 11:59:16 +0100

    Remove state and country

--- form.js
+++ form.js
@@ -51,32 +51,6 @@
                 }
             },
             
-            country: function() {
-                switch(settings.locale) {
-                  case "fr":
-                    return "Votre pays";
-                    break;
-                  case "es":
-                    return "Tu país";
-                    break;
-                  default:
-                    return "Your country";
-                }
-            },
-            
-            state: function() {
-                switch(settings.locale) {
-                  case "fr":
-                    return "Votre région";
-                    break;
-                  case "es":
-                    return "Tu región";
-                    break;
-                  default:
-                    return "Your state";
-                }
-            },
-            
             signin: function() {
                 switch(settings.locale) {
                   case "fr":
@@ -218,35 +192,6 @@
                 return output;
             },
             
-            countries: function() {
-                
-                output = '<div class="field"> \
-                            <select id="country" name="country" required="required">';
-                
-                $.getJSON( "https://discutea.fr/api/irc/form/country", function( data ) {
-               
-                    if (data.length > 0){
-
-                        countrieslist = '<option value="">' + trans.country() + '</option>';
-                        
-                        $.each( data, function( key, val ) {
-                            countrieslist += '<option value="' + val.id + '"';
-                            
-                          if ( (settings.cookies) && (Cookies.get('Country')) && (val.id == Cookies.get('Country')) ) {
-                            countrieslist += ' selected '; 
-                          }
-
-                            countrieslist += '>' + val.country + '</option>';
-                        });
-
-                        $("#country").html(countrieslist);
-                    }
-                });
-                
-                
-                output += '</select></div>';
-                return output;
-            },
             
             
             assembler: function() {       
@@ -257,13 +202,6 @@
                 output += this.username();
                 output += this.year();
                 output += this.gender();
-                output += this.countries();
-                
-                output += '<div id="field_region" class="field"> \
-                             <select id="region" name="region"> \
-                               <option value="">' + trans.state() + '</option> \
-                             </select> \
-                           </div>';
                 
                 output += "<div class='submit'> \
                              <input type='submit' class='button-chat' value='" + trans.signin() + "' /> \
@@ -274,26 +212,7 @@
             
         }
         
-        var events = {
-            onCountryChange: function( country_id) {
-                $("#field_region").hide();
-                $field = $("#region");
-
-                $.getJSON( "https://discutea.fr/api/irc/form/regions/"+country_id, function( data ) {
-                    $field.empty();
-                    if (data.length > 0){
-                        $.each( data, function( key, val ) {
-                          if ( (settings.cookies) && (Cookies.get('District')) && (val.id == Cookies.get('District')) ) {
-                            $field.append('<option value="' + val.id + '" selected >' + val.region + '</option>');
-                          } else {
-                            $field.append('<option value="' + val.id + '">' + val.region + '</option>');
-                          }                            
-                        });
-                        $("#field_region").show();
-                    }
-                });
-            },
-            
+        var events = {            
             onSubmit: function( e ) {
                 e.preventDefault();
  
@@ -306,21 +225,12 @@
                     gender = 'F';
                 }
                 
-                country = $("#country").find('option:selected').text();
-                region = $("#region").find('option:selected').text();
-                
                 url = trans.appletUrl();
 
                 if (settings.post === false) {
                   url += "?nick=" + nick;
                   url += "&age=" + age;
                   url += "&sex=" + gender;
-                  url += "&country=" + country;
-
-                  if (region != '') {
-                    url += "&district=" + region;
-                  }
-                
                   url += "&channel=#" + trans.channel();
                 
                   window.location.href = url;
@@ -333,10 +243,6 @@
                   form.append('<input type="hidden" name="tchat[username]" value="'+nick+'">');
                   form.append('<input type="hidden" name="tchat[age]" value="'+age+'">');
                   form.append('<input type="hidden" name="tchat[sexe]" value="'+gender+'">');
-                  form.append('<input type="hidden" name="tchat[pays]" value="'+country+'">');
-                  if (region != '') {
-                    form.append('<input type="hidden" name="tchat[region]" value="'+region+'">');
-                  }
                   form.append('<input type="hidden" name="tchat[channel]" value="'+trans.channel()+'">');
                   
                   $(form).appendTo('body').submit();
@@ -344,20 +250,7 @@
             }
         }
 
-        this.html( html.assembler() );
-        $("#field_region", this).hide();
-
-        if ( (settings.cookies) && (Cookies.get('Country')) ) {
-            events.onCountryChange( Cookies.get('Country') ); 
-        }
-
-        $("#country", this).change(function() {
-          country = $(this).val();
-          events.onCountryChange(country);
-           if (settings.cookies) {
-               Cookies.set('Country', country, { expires: 365 });  
-           }
-        });  
+        this.html( html.assembler() ); 
         
         if (settings.cookies){
           $("#username", this).blur(function(){
@@ -371,10 +264,6 @@
         
           $("#gender", this).change(function(){
             Cookies.set('Sex', $("input[type='radio']:checked", this).val(), { expires: 365 })
-          });
-          
-          $("#region", this).change(function() {
-            Cookies.set('District', $(this).val(), { expires: 365 });  
           });
         }
         
